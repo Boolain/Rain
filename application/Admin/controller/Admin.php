77224994e21a3cbb10049098a9cbaa75c3065aaf
Boolain <Boolain@163.com>
2018-03-15 19:37:53 +0800

    TP5.0

---
+++ application/Admin/controller/Admin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\Admin\controller;
+
+use think\Controller;
+use think\Request;
+use think\View;
+use think\Db;
+
+
+class Admin EXTENDS Controller{
+	public function index(){
+		
+	}
+}
